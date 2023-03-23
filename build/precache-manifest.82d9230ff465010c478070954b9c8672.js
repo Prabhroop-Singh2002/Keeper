15fa8e06af350eb86d222f5b49af930adaf7ac88
Prabhroop-Singh2002 <114666573+Prabhroop-Singh2002@users.noreply.github.com>
2023-03-23 23:08:07 +0530

    Add files via upload

---
+++ build/precache-manifest.82d9230ff465010c478070954b9c8672.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4260d7aabc315f0466b5c9225cdc996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4530b8c5089b9f78e901",
+    "url": "/static/js/2.6a20179d.chunk.js"
+  },
+  {
+    "revision": "0ffc13be7b10ac922ef7",
+    "url": "/static/js/main.6d6fa2e0.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
